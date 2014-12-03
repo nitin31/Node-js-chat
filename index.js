9ef11f47eb2bf541b186b34014816f0d1862298f
nitin31 <nitinsharma0389@gmail.com>
2014-12-02 23:14:47 -0500

    add index.html

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.get('/', function(request, response) {
   response.sendFile('index.html', {"root": __dirname});
 });
 
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected');
-//   });
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 
-// });
+});
 
 app.listen(app.get('port'));
